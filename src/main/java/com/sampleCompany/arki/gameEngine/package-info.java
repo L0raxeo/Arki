206eafc23fab373a390cf6383ccfc30e953a30ca
Lorcan <67399230+L0raxeo@users.noreply.github.com>
2021-12-25 21:04:55 -0600

    package info

---
+++ src/main/java/com/sampleCompany/arki/gameEngine/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The game engine resides in this package, and it is HIGHLY recommended
+ * that this package goes untouched. Changing any code within this package
+ * could cause bugs in the game engine, which would ultimately break it.
+ */
+package com.sampleCompany.arki.gameEngine;
---
+++ src/main/java/com/sampleCompany/game/sampleGame/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This module is where the game developers develop their game.
+ * All Game code goes in this package.
+ */
+package com.sampleCompany.game.sampleGame;
